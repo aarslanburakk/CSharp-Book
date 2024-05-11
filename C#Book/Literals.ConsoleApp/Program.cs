ecd1a88f41a102b1c5923fbde58bf36f52489ab5
Burak Arslan <arslan.burak0909@gmail.com>
2024-05-11 23:09:32 +0300

    Version 1.0

---
+++ C#Book/Literals.ConsoleApp/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+
+uint ui = 4000;
+Console.WriteLine(ui);
+// ifadesinde, ui adlı bir değişken uint türünde tanımlanır ve bu değişkene 5 değeri atanır. Sonundaki "U" veya "u" harfi, bu sayının unsigned (işaretsiz) olduğunu belirtir.
+// C#'da uint literalleri, sonuna "U" veya "u" eklenerek veya uint veri türü sınırları içindeki bir tam sayı değeri kullanılarak tanımlanabilir. Bu, C#'ın dil özelliklerinden biridir ve özellikle bellek kullanımı ve matematiksel işlemler için faydalıdır.
+// Yani, uint türü 0 ile 4294967295 arasında (dahil) değer alabilir.
+
+
+
+int a = 0;
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine(a);
+Console.WriteLine(max);
+Console.WriteLine(min);
+//C#'da, int bir tam sayı veri türüdür ve "integer"ın kısaltmasıdır. int, genellikle imzalı bir tamsayıyı temsil etmek için kullanılır, yani hem pozitif hem de negatif tam sayıları içerebilir.
+
+// int türü, genellikle bellekte 4 byte (32 bit) yer kaplar ve bu nedenle yaklaşık olarak -2147483648 milyar ile 2147483647 milyar arasındaki değerleri temsil edebilir.
+
+
+byte b = 127;
+
+// Bu durumda, byte C#'da bir veri türüdür ve genellikle 8 bitlik imzalı olmayan (unsigned) tamsayıları temsil etmek için kullanılır. byte, 0 ile 255 arasındaki değerleri alabilir.
+
+// Ancak, 127 bir int türündedir ve byte'a otomatik olarak dönüştürülmez çünkü byte'ın aralığı 0 ile 255 arasındadır. Dolayısıyla, bu durumda bir derleme hatası alabilirsiniz. byte türünde bir değişkene int türünden bir değer atanırken açıkça dönüşüm yapılması gerekmektedir.
+
+short s = 127;
+
+
+// Bu durumda, short C#'da bir veri türüdür ve genellikle kısa tamsayıları temsil etmek için kullanılır. short, genellikle 16 bitlik bir tamsayıdır ve -32,768 ile 32,767 arasındaki değerleri alabilir.
+
+ushort us = 127;
+
+// Bu durumda, ushort C#'da bir veri türüdür ve "unsigned short"un kısaltmasıdır. ushort, pozitif tamsayıları temsil etmek için kullanılır ve 0'dan başlayarak 65535'e kadar olan değerleri alabilir.
+
+sbyte sb = 127;
+
+// Bu ifadede, sbyte C#'da bir veri türüdür ve "signed byte"ın kısaltmasıdır. sbyte, 8 bitlik bir imzalı (signed) tamsayıyı temsil eder, bu da -128 ile 127 arasındaki alabilir.
+
+decimal m = 30.5M;
+// Bu ifadede, decimal C#'da bir veri türüdür ve ondalık sayıları temsil etmek için kullanılır. decimal, yüksek hassasiyet gerektiren finansal veya hesaplamalı uygulamalarda tercih edilir.
+
+double d = 30.5D;
+// Bu ifadede, double C#'da bir veri türüdür ve kayan noktalı sayıları (ondalık sayılar) temsil etmek için kullanılır. double, genellikle yüksek hassasiyet gerektirmeyen hesaplamalarda kullanılır.
+
+float f = 30.5F;
+float maxfloat = float.MaxValue;
+float minfloat = float.MinValue;
+Console.WriteLine(f);
+Console.WriteLine(maxfloat);
+Console.WriteLine(minfloat);
+
+// Bu ifadede, float C#'da bir veri türüdür ve kayan noktalı sayıları (ondalık sayılar) temsil etmek için kullanılır. float, genellikle daha düşük hassasiyete sahip olmasına rağmen, daha az bellek kullanımı gerektiren durumlarda double'dan daha yaygın olarak kullanılır.
+
+long l = 5;
+long maxlong = long.MaxValue;
+long minlong = long.MinValue;
+
+// Bu ifadede, long C#'da bir veri türüdür ve genellikle büyük tamsayı değerlerini temsil etmek için kullanılır. long, genellikle int'ten daha büyük aralıklarda değerler saklamak için kullanılır.
+
+
+ulong ul = 5UL;
+// Bu ifadede, ulong C#'da bir veri türüdür ve "unsigned long"un kısaltmasıdır. ulong, pozitif tamsayıları temsil etmek için kullanılır ve long türünden daha büyük pozitif tamsayılar saklayabilir. ulong, yalnızca pozitif değerleri temsil eder ve 0'dan başlayarak 8 byte'lık bir aralıkta değer alır.
+
+
+
+
+
+bool val = true;
+// Bu durumda, bool C#'da bir veri türüdür ve sadece iki değeri alabilir: true (doğru) veya false (yanlış). Mantıksal operasyonlar ve karar yapıları gibi durumlarda sıklıkla kullanılır.
+
+string  valStr = "hello, this is a string literal";
+
+string valStrOne  = $"{ul} değerini yazılabilir";
+
+// Bu ifadede, string C#'da bir veri türüdür ve metin verilerini temsil etmek için kullanılır. string, karakter dizilerini saklamak için kullanılır ve çift tırnak içinde belirtilir.
+
---
+++ C#Book/Operators.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿int a = 10;
+int b = 5;
+int sum = a + b; // Toplama operatörü
+int difference = a - b; // Çıkarma operatörü
+int product = a * b; // Çarpma operatörü
+int quotient = a / b; // Bölme operatörü
+int remainder = a % b; // Mod alma operatörü
+
+
+int x = 10;
+int y = 5;
+bool isEqual = (x == y); // Eşitlik karşılaştırma operatörü
+bool isNotEqual = (x != y); // Eşitsizlik karşılaştırma operatörü
+bool isGreater = (x > y); // Büyüklük karşılaştırma operatörü
+bool isLess = (x < y); // Küçüklük karşılaştırma operatörü
+
+int num = 10; // Atama operatörü
+
+
+bool condition1 = true;
+bool condition2 = false;
+bool result1 = condition1 && condition2; // Ve operatörü
+bool result2 = condition1 || condition2; // Veya operatörü
+bool result3 = !condition1; // Değil operatörü
+
+
+var XT = true;
+var YF = false;
+var val = XT && YF; // Sonuç true döner.
+var val1 =  XT && YF; // Sonuç false döner (y değerlendirilir).
+var val2  = YF && XT; // Sonuç false döner (x değerlendirilmez).
+var val3 = YF && YF; // Sonuç false döner (sağdaki y değerlendirilmez).    
+Console.WriteLine(val2);
+
+//  Ternary Operator
+
+int age = 18;
+
+string result = (age >= 18) ? "Yetişkin" : "Çocuk";
+Console.WriteLine("Bu kişi bir " + result + "dur.");
+
+// condition ? expression_if_true : expression_if_false;
+// Eğer bir koşul doğru ise, bir ifadeyi değerlendir; aksi takdirde, başka bir ifadeyi değerlendir.
